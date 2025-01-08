65960c86d0ab9dc46c1ec1947674e4af5ac487c1
wagneradrian <45693531+wagneradrian@users.noreply.github.com>
2025-01-08 11:59:53 +0100

    first commit

---
+++ comics/navigation.js
@@ -0,0 +1,33 @@
+// Get the current page (e.g., episode1.html)
+const currentPage = window.location.pathname.split('/').pop();
+
+// Fetch episode metadata from episodes.json
+fetch('/comics/episodes.json')
+    .then(response => response.json())
+    .then(data => {
+        const episodes = data.episodes;
+        const currentIndex = episodes.findIndex(ep => ep.file === currentPage);
+
+        // Get buttons
+        const prevButton = document.getElementById('prevButton');
+        const nextButton = document.getElementById('nextButton');
+
+        // Set Previous Button
+        if (currentIndex > 0) {
+            prevButton.onclick = () => {
+                window.location.href = episodes[currentIndex - 1].file;
+            };
+        } else {
+            prevButton.disabled = true; // Disable if no previous
+        }
+
+        // Set Next Button
+        if (currentIndex < episodes.length - 1) {
+            nextButton.onclick = () => {
+                window.location.href = episodes[currentIndex + 1].file;
+            };
+        } else {
+            nextButton.disabled = true; // Disable if no next
+        }
+    })
+    .catch(error => console.error('Error loading episodes:', error));
